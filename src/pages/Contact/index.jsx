a260301d635110a0a18dd5646632375adfff0d95
Nathália Veneziano <nathaliaveneziano@icloud.com>
2023-07-14 16:32:43 -0300

    creating contact page

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,7 +1,68 @@
+import { Icon } from '../../components';
+import data from '../../data';
 import './contact.css';
 
 function Contact() {
-  return <div>Contact</div>;
+  const { socialMedia } = data;
+
+  return (
+    <section className="contact container section" id="contact">
+      <h2 className="section__title">Contatos</h2>
+
+      <div className="contact__container grid">
+        <div className="contact__info">
+          <h3 className="contact__title">Let's talk about everthing!</h3>
+          <p className="contact_details">Don't like forms? Send me an email.</p>
+          <div className="contact__social-group">
+            {socialMedia.map(({ id, url, title, icon }) => (
+              <a href={url} className="contact__social" title={title} key={id}>
+                <Icon icon={icon} typeIcon="fa6" />
+                {title}
+              </a>
+            ))}
+          </div>
+        </div>
+
+        <form className="contact__form">
+          <div className="contact__form-group">
+            <div className="contact__form-div">
+              <input
+                type="text"
+                className="contact__form-input"
+                placeholder="Digite seu nome"
+              />
+            </div>
+            <div className="contact__form-div">
+              <input
+                type="email"
+                className="contact__form-input"
+                placeholder="Digite seu e-mail"
+              />
+            </div>
+          </div>
+          <div className="contact__form-div">
+            <input
+              type="text"
+              className="contact__form-input"
+              placeholder="Digite um assunto"
+            />
+          </div>
+          <div className="contact__form-div contact__form-area">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              className="contact__form-input"
+              placeholder="Escreva sua mensagem"></textarea>
+          </div>
+          <button type="submit" className="btn">
+            Enviar Mensagem
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default Contact;
